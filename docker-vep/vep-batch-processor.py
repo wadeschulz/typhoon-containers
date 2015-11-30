2ce75d9a5509a8bc8de21ad4d5eba17e8fab8949
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2015-11-30 14:10:29 -0500

    add vep batch script

---
+++ docker-vep/vep-batch-processor.py
@@ -0,0 +1,54 @@
+__author__    = 'Tommy Durant, Wade Schulz'
+__email__     = 'thomas.sakrison.durant@gmail.com, wade.schulz@gmail.com'
+__version__   = '1.0'
+__date__      = '11/26/2015'
+__pyver__     = '2.7'
+
+"""
+#######################  SciCloneClient ########################################
+ 
+            Python based SciClone bulk runner
+ 
+            > SciClone python client for bulk run of VCF files
+            > Input: Directory with VCF files, output directory, and number of
+                      iterations to run SciClone
+            > Output: Clone clusterTable and clusterPlot, summary statistics
+ 
+################################################################################
+"""
+
+##### Import python modules
+################################################################################
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+################################################################################
+##### Classes
+################################################################################
+
+os.system('cls' if os.name == 'nt' else 'clear')
+script_base = '/home/bio/ensembl-tools-release-82/scripts/variant_effect_predictor/'
+
+# Enter input/output paths
+base_path = raw_input('Enter data base directory: ')
+
+input_path = join(base_path, "input")
+out_base = join(base_path, "output")
+
+path_list = [out_base]
+for directory in path_list:
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+input_files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
+script_path = join(script_base, "variant_effect_predictor.pl")
+
+for file in input_files:
+  name = file.split('.')[0]
+  print "Evaluating specimen: " + name
+  
+  out_path = join(out_base, name + '.out')
+  # port 3337 for hg19 database
+  os.system('perl ' + script_path + ' -i ' + file + ' -o ' + out_path + ' --database --fork 5 --hgvs --symbol --assembly GRCh37 --port 3337 --species homo_sapiens')
