da711c90ec4cc1980f8f3ab2d6e9df04752e33e4
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2016-01-08 21:22:56 -0500

    add cgquery/gtdownload with wrapper container

---
+++ docker-cghub/download-wrapper.py
@@ -0,0 +1,5 @@
+import os, sys
+
+analysisId = sys.argv[1]
+os.system('cgquery -o /mnt/tcga/' + analysisId + '.xml "analysis_id=' + analysisId + '"')
+os.system('gtdownload -v -p /mnt/tcga/download -c /mnt/tcga/credentials --max-children 16 /mnt/tcga/' + analysisId + '.xml')
