55c7cc1a72fbfb2dd7c89ea4ac0a4d029774e783
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2016-01-10 16:55:16 -0500

    change cgquery xml file naming

--- docker-cgdownload/cgdownload.py
+++ docker-cgdownload/cgdownload.py
@@ -3,4 +3,4 @@ import os, sys
 analysisId = sys.argv[1]
 
 os.system('/bin/bash -c \'cgquery -o /mnt/data/temp.xml analysis_id="' + analysisId + '"\'')
-os.system('/bin/bash -c \'gtdownload -vvvv -p /mnt/data/download -c /mnt/data/credentials --max-children 16 /mnt/data/temp.xml\'')
+os.system('/bin/bash -c \'gtdownload -vvvv -p /mnt/data/download -c /mnt/data/credentials --max-children 16 /mnt/data/' + analysisId + '.xml\'')
