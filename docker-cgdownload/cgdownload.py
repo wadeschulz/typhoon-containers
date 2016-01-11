41f97b360e93cbf06dd21db63fb665163e93c196
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2016-01-10 19:33:15 -0500

    fix bug

--- docker-cgdownload/cgdownload.py
+++ docker-cgdownload/cgdownload.py
@@ -2,5 +2,5 @@ import os, sys
 
 analysisId = sys.argv[1]
 
-os.system('/bin/bash -c \'cgquery -o /mnt/data/' + analysis_id + '.xml analysis_id="' + analysisId + '"\'')
+os.system('/bin/bash -c \'cgquery -o /mnt/data/' + analysisId + '.xml analysis_id="' + analysisId + '"\'')
 os.system('/bin/bash -c \'gtdownload -v -p /mnt/data/download -c /mnt/data/credentials --max-children 16 /mnt/data/' + analysisId + '.xml\'')
